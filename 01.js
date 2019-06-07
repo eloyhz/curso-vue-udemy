f3edf201daaf9713d7a1a02c985dce2235500cea
Eloy Hernández <eloyhz@gmail.com>
2019-06-06 20:10:41 -0500

    Vue basico, ciclos v-for y condicionales v-if

---
+++ 01.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    titulo: 'Hola Vue',
+    frutas: [
+      {nombre: 'Manzana', cantidad: 3},
+      {nombre: 'Pera', cantidad: 0},
+      {nombre: 'Uva', cantidad: 5},
+    ]
+  }
+});
