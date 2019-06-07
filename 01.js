ad3c0ed2d3ff151fa02b857fc1e2ba44bd234c3d
Eloy Hernández <eloyhz@gmail.com>
2019-06-06 20:24:28 -0500

    Enlace de datos v-model y eventos v-on

--- 01.js
+++ 01.js
@@ -6,6 +6,14 @@ const app = new Vue({
       {nombre: 'Manzana', cantidad: 3},
       {nombre: 'Pera', cantidad: 0},
       {nombre: 'Uva', cantidad: 5},
-    ]
+    ],
+    nuevaFruta: ''
+  },
+  methods: {
+    agregarFruta() {
+      this.frutas.push({
+        nombre: this.nuevaFruta, cantidad: 0
+      });
+    }
   }
 });
